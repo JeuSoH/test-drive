5d1cb27ddcbef4a30ff30fdb334bda1816ad1e75
chyngyz-kgz <chyngyz.kg.na.ak@gmail.com>
2021-04-28 18:23:19 +0600

    Added SignIn page

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
+import SignIn from './components/SignIn/SignIn';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={SignIn} />
             </Switch>
         </BrowserRouter>
     );
